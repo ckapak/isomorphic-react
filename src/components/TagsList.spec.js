6f1987c020c8a37aa06631666349957d9af94f29
ckapak <christinekapak@googlemail.com>
2020-04-28 17:44:12 +0100

    snapshot

---
+++ src/components/TagsList.spec.js
@@ -0,0 +1,3 @@
+import React from 'react'
+import Tagslist from '.TagsList'
+import renderer from 'react-test-renderer'
--- src/sagas/fetch-questions-saga.js
+++ src/sagas/fetch-questions-saga.js
@@ -5,15 +5,15 @@ import fetch from 'isomorphic-fetch';
  * questions in response to a particular view being loaded
  */
 export default function * () {
-    while (true) {
-        /**
+  while (true) {
+    /**
          * Wait for a request to fetch questions, then fetch data from the API and notify the application
          * that new questions have been loaded.
          */
-        yield take(`REQUEST_FETCH_QUESTIONS`);
-        const raw = yield fetch('/api/questions');
-        const json = yield raw.json();
-        const questions = json.items;
-        yield put({type:`FETCHED_QUESTIONS`,questions});
-    }
+    yield take(`REQUEST_FETCH_QUESTIONS`);
+    const raw = yield fetch('/api/questions');
+    const json = yield raw.json();
+    const questions = json.items;
+    yield put({type:`FETCHED_QUESTIONS`,questions});
+  }
 }
